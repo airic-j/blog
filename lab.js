9ab67ebd24143c7fe51e8695670c2e5d8ff8d0d4
Eric Johnson <eric@ericslab.com>
2016-05-10 18:34:53 -0700

    update styles for blog, add sticky nav

--- lab.js
+++ lab.js
@@ -1,49 +1,72 @@
 $('document').ready(function () {
-	console.log('lab.js loads');
+  console.log('lab.js loads');
 
 	// hide projects at initial page load
-	$('#projects').hide();
+  $('section').not('#about').hide();
 
-	var projects = [];
+  var projects = [];
 
-	function Project(x) {	
-		this.title = x.title;
-		this.date = x.date;
-		this.description = x.description;
-		this.link = x.link;
-	}
+  function Project(x) {
+    this.title = x.title;
+    this.date = x.date;
+    this.description = x.description;
+    this.link = x.link;
+    this.image = x.image;
+  }
 
-	Project.prototype.toHtml = function() {
-		var $newProject = $('.prototype').clone();
-		
-		$newProject.find('#title').html(this.title)
-		$newProject.find('#date').html(this.date)
-		$newProject.find('#link').html(this.link)
-		$newProject.find('#description').html(this.description)
+  Project.prototype.toHtml = function() {
+    var $newProject = $('.prototype').clone();
+    $newProject.find('#title').html(this.title);
+    $newProject.find('#date').html(this.date);
+    $newProject.find('#link a.attr("href")').html(this.link);
+    $newProject.find('#link a').html(this.link);
+    $newProject.find('#description').html(this.description);
+    // TODO get this working
+    $newProject.find('#projectImage.attr("src")').html(this.image);
 
-	  $newProject.removeClass('prototype');
-		return $newProject;
-	}
+    $newProject.removeClass('prototype');
+    return $newProject;
+  };
 
-	projectData.forEach(function(projectData){
-		projects.push(new Project(projectData));
-		console.log('push project to array');
-	});
+  projectData.forEach(function(projectData){
+    projects.push(new Project(projectData));
+    console.log(projectData);
+    console.log('push project to array');
+  });
 
-	projects.forEach(function(project){
-		$('#projects').append(project.toHtml());
-		console.log('append project');
-	});
+  projects.forEach(function(project){
+    $('#projects').append(project.toHtml());
+    console.log('append project');
+  });
+
+	//sticky nav
+
+  var topOfNav = $('.nav').offset().top;
+  var stickyNav = function() {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > topOfNav) {
+      $('nav').addClass('stickyNav').removeClass('nav');
+      console.log('start sticky');
+    } else {
+      $('nav').removeClass('stickyNav').addClass('nav');
+    }
+  };
+
+  stickyNav();
+
+  $(window).scroll(function() {
+    stickyNav();
+  });
 
 
 	// change visible tab by click on nav links
 
-	$('nav a').on('click', function() {
-		$tabClicked = $(this).data('tab');
-		console.log('clicked ' + $tabClicked);
-		$(this).addClass('selected').siblings().removeClass('selected');
-		$('section').hide();
-		$('#' + $tabClicked).show();
-	})
+  $('nav a').on('click', function() {
+    $tabClicked = $(this).data('tab');
+    console.log('clicked ' + $tabClicked);
+    $(this).addClass('selected').siblings().removeClass('selected');
+    $('section').hide();
+    $('#' + $tabClicked).show();
+  });
 
 });
--- projectData.js
+++ projectData.js
@@ -1,22 +1,25 @@
-console.log("data loads");
+console.log('data loads');
 
 var projectData = [
-	{
-		title: 'SF Casual Carpool',
-		date: '2015-01-01',
-		description: 'Bacon ipsum dolor amet sirloin salami drumstick, tenderloin kielbasa kevin capicola. Rump turducken sausage filet mignon. Doner beef ribs sirloin capicola chicken frankfurter jerky sausage tenderloin tongue corned beef shankle. Kielbasa jowl ball tip, beef ribs ribeye landjaeger chicken brisket swine prosciutto drumstick ground round ham cupim pastrami. Venison tri-tip bresaola turkey spare ribs jowl fatback strip steak frankfurter beef ribs meatloaf turducken capicola. Pork landjaeger meatball, kevin corned beef beef ribs pork chop.',
-		link: 'sfcasualcarpool.com'
-	},
-	{
-		title: 'Barbareno',
-		date: '2016-01-01',
-		description: 'Bacon ipsum dolor amet sirloin salami drumstick, tenderloin kielbasa kevin capicola. Rump turducken sausage filet mignon. Doner beef ribs sirloin capicola chicken frankfurter jerky sausage tenderloin tongue corned beef shankle. Kielbasa jowl ball tip, beef ribs ribeye landjaeger chicken brisket swine prosciutto drumstick ground round ham cupim pastrami. Venison tri-tip bresaola turkey spare ribs jowl fatback strip steak frankfurter beef ribs meatloaf turducken capicola. Pork landjaeger meatball, kevin corned beef beef ribs pork chop.',
-		link: 'barbareno.com'
-	},
-	{
-		title: 'The Trappist',
-		date: '2016-01-01',
-		description: 'Bacon ipsum dolor amet sirloin salami drumstick, tenderloin kielbasa kevin capicola. Rump turducken sausage filet mignon. Doner beef ribs sirloin capicola chicken frankfurter jerky sausage tenderloin tongue corned beef shankle. Kielbasa jowl ball tip, beef ribs ribeye landjaeger chicken brisket swine prosciutto drumstick ground round ham cupim pastrami. Venison tri-tip bresaola turkey spare ribs jowl fatback strip steak frankfurter beef ribs meatloaf turducken capicola. Pork landjaeger meatball, kevin corned beef beef ribs pork chop.',
-		link: 'thetrappist.com'
-	}
-]
+  {
+    title: 'SF Casual Carpool',
+    date: '2015-01-01',
+    description: 'Bacon ipsum dolor amet sirloin salami drumstick, tenderloin kielbasa kevin capicola. Rump turducken sausage filet mignon. Doner beef ribs sirloin capicola chicken frankfurter jerky sausage tenderloin tongue corned beef shankle. Kielbasa jowl ball tip, beef ribs ribeye landjaeger chicken brisket swine prosciutto drumstick ground round ham cupim pastrami. Venison tri-tip bresaola turkey spare ribs jowl fatback strip steak frankfurter beef ribs meatloaf turducken capicola. Pork landjaeger meatball, kevin corned beef beef ribs pork chop.',
+    link: 'sfcasualcarpool.com',
+    image: 'casualcarpool'
+  },
+  {
+    title: 'Barbareno',
+    date: '2016-01-01',
+    description: 'Bacon ipsum dolor amet sirloin salami drumstick, tenderloin kielbasa kevin capicola. Rump turducken sausage filet mignon. Doner beef ribs sirloin capicola chicken frankfurter jerky sausage tenderloin tongue corned beef shankle. Kielbasa jowl ball tip, beef ribs ribeye landjaeger chicken brisket swine prosciutto drumstick ground round ham cupim pastrami. Venison tri-tip bresaola turkey spare ribs jowl fatback strip steak frankfurter beef ribs meatloaf turducken capicola. Pork landjaeger meatball, kevin corned beef beef ribs pork chop.',
+    link: 'barbareno.com',
+    image: 'barbareno'
+  },
+  {
+    title: 'The Trappist',
+    date: '2016-01-01',
+    description: 'Bacon ipsum dolor amet sirloin salami drumstick, tenderloin kielbasa kevin capicola. Rump turducken sausage filet mignon. Doner beef ribs sirloin capicola chicken frankfurter jerky sausage tenderloin tongue corned beef shankle. Kielbasa jowl ball tip, beef ribs ribeye landjaeger chicken brisket swine prosciutto drumstick ground round ham cupim pastrami. Venison tri-tip bresaola turkey spare ribs jowl fatback strip steak frankfurter beef ribs meatloaf turducken capicola. Pork landjaeger meatball, kevin corned beef beef ribs pork chop.',
+    link: 'thetrappist.com',
+    image: 'trappist'
+  }
+];
